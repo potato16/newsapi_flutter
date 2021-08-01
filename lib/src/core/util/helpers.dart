51519cdb9663017284de6d735c915ae2d1cae61b
Thao Han <nosaitx3@gmail.com>
2021-08-01 16:29:59 +0700

    convert datetime

--- lib/src/core/util/helpers.dart
+++ lib/src/core/util/helpers.dart
@@ -1,20 +1,17 @@
 import 'package:intl/intl.dart';
 
-// const String dateTimePattern = 'EEE, dd MMM yyyy HH:mm:ss Z';
-// Remove timezone for now because intl does not implement it yet
-// So we assume all time zone value will be +0000
-const String dateTimePattern = 'EEE, dd MMM yyyy HH:mm:ss';
+const String datePattern = 'yyyy-MM-dd';
+const String timePattern = 'HH:mm:ss';
 
-/// input format: Wed, 19 Sep 2018 09:30:55 +0000
+/// input format: 2021-07-22T21:06:57Z
 /// return format: 19 Sep 2018 if time is pass 24h
 /// return format: 1 hours ago if time not pass 24h
 /// return format: now if time is not pass 1h
 String convertDateTimeFormat(String input) {
-  if (input.endsWith('+0000')) {
-    input = input.substring(0, input.length - 5);
-  }
-  final date = stringToDateTime(input);
-  if (date == null) {
+  DateTime date;
+  try {
+    date = DateTime.parse(input).toLocal();
+  } catch (e) {
     return '';
   }
   final now = DateTime.now();
@@ -32,20 +29,10 @@ String convertDateTimeFormat(String input) {
   }
 }
 
-/// input format: Wed, 19 Sep 2018 09:30:55 +0000
-DateTime? stringToDateTime(String input) {
-  if (input.endsWith('+0000')) {
-    input = input.substring(0, input.length - 5);
-  }
-  DateFormat format = DateFormat(dateTimePattern);
-  try {
-    return format.parse(input);
-  } catch (e) {
-    return null;
-  }
-}
-
-/// output format: Wed, 19 Sep 2018 09:30:55 +0000
+/// output format:2021-07-22T21:06:57Z
 String dateTimeToString(DateTime input) {
-  return DateFormat(dateTimePattern).format(input);
+  final dt = input.toUtc();
+  final d = DateFormat(datePattern).format(dt);
+  final t = DateFormat(timePattern).format(dt);
+  return '${d}T${t}Z';
 }
---
+++ test/unit_tests/datetime_convert_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:newsapi_flutter/src/core/util/helpers.dart';
+
+void main() {
+  test('convert 2021-07-22T21:06:57Z to 22 Jul 2021', () {
+    final input = '2021-07-22T21:06:57Z';
+    final want = '23 Jul 2021';
+    final got = convertDateTimeFormat(input);
+    expect(got, want);
+  });
+  test('convert time pass fews hours ago', () {
+    final time = DateTime.now().subtract(Duration(minutes: 30, hours: 2));
+    final input = dateTimeToString(time);
+    final want = '2 hours ago';
+    final got = convertDateTimeFormat(input);
+    expect(got, want);
+  });
+  test('convert time pass just now', () {
+    final time = DateTime.now().subtract(Duration(
+      minutes: 30,
+    ));
+    final input = dateTimeToString(time);
+    final want = 'now';
+    final got = convertDateTimeFormat(input);
+    expect(got, want);
+  });
+  test('convert convertDateTimeFormat with invalid input', () {
+    final input = 'Wed, 19 Sep 2018 09:30';
+    final want = '';
+    final got = convertDateTimeFormat(input);
+    expect(got, want);
+  });
+}
